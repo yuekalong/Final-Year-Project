ea6edfbe0809162715a2ed3bcf70ceadb12581fe
yuekalong <yuekalong199972@gmail.com>
2020-09-09 18:26:38 +0800

    build layout of chatroom and fix the git ignore

---
+++ Final Year Project/Assets/Scripts/ChatroomPanel/ChatroomInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
