3e2c26c79cc54889f1cff7e5dca44ff3760ecefe
charlie2299 <charliewong.wkc@gmail.com>
2021-01-06 16:32:59 +0800

    Straight Draw Line Test

---
+++ Final Year Project/Assets/Balls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    GameObject obj;
+    void Awake()
+    {
+        obj = GameObject.Find("GameObject");
+    }
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+        obj.GetComponent<DrawLine>().tempPoint.x=transform.position.x;
+        obj.GetComponent<DrawLine>().tempPoint.y=transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
