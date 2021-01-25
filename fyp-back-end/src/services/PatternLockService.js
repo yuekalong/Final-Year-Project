593a1de60e509b44d8c68ae522dc850b49acef15
yuekalong <yuekalong199972@gmail.com>
2021-01-25 15:17:54 +0800

    fix(pattern lock validation): fix the desc problem in back-end

--- fyp-back-end/src/services/PatternLockService.js
+++ fyp-back-end/src/services/PatternLockService.js
@@ -2,10 +2,14 @@ const knex = require("knex")(require("../../knexfile.js")["development"]);
 
 module.exports = {
   validInput: async function (lockID, input) {
+    let arr = JSON.parse(input);
+
     const order = (
       await knex("pattern_lock").first("order").where("id", "=", lockID)
     ).order;
 
-    return order == input;
+    return (
+      order == JSON.stringify(arr) || order == JSON.stringify(arr.reverse())
+    );
   },
 };
