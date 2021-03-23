173bbd9b6f2a67d199075bb2e084d7c2c73a009b
yuekalong <yuekalong199972@gmail.com>
2021-03-23 18:04:28 +0800

    ready

--- Final Year Project/Assets/PlatformDefines.cs
+++ Final Year Project/Assets/PlatformDefines.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public static class PlatformDefines
 {
-    public const string apiAddress = "https://soul-tracer-backend.herokuapp.com";
-    public const string socketAddress = "ws://soul-tracer-backend.herokuapp.com";
+    public const string apiAddress = "http://54.198.90.78:3000";
+    public const string socketAddress = "ws://54.198.90.78:3000";
 }
