03a6358df5b6c9c95487ede9291e589ba1f97a59
charlie2299 <charliewong.wkc@gmail.com>
2021-04-25 12:10:32 +0800

    button testing

--- Final Year Project/Assets/Scripts/Treasure.cs
+++ Final Year Project/Assets/Scripts/Treasure.cs
@@ -15,54 +15,32 @@ public class Treasure : MonoBehaviour
 
     public InputField input;
 
+    public Button but;
+
     private string type;
 
     bool  show=false;
     void Start()
     {
-        
+        if(PlayerPrefs.GetInt("scanned",1)==1)
+        {
+            but.interactable=false;
+            StartCoroutine(CountDown());
+        }
 
     }
 
 
-    public void onclick()
+    public void onclick() 
     {
         //inputfield.SetActive(!show);
         //show=!show;
         SceneManager.LoadScene("AR Testing");
-    }
-    public void submit()
-    {
-        //StartCoroutine(CheckCode());
+        PlayerPrefs.SetInt("scanned",1);
     }
 
-    /*IEnumerator CheckCode(){
-    while(true)
-    {
-        WWWForm form = new WWWForm();
-        string user_input = input.text;
-        
-        form.AddField("input",user_input);
-
-        UnityWebRequest req = UnityWebRequest.Post(PlatformDefines.apiAddress + "/treasure/"+PlayerPrefs.GetString("game_id", "1"),form);
-
-        // stop the function and return the state to Login(), if access this function again will start from here
-        yield return req.SendWebRequest();
-
-        JSONNode res = JSON.Parse(req.downloadHandler.text);
-        JSONNode data = res["data"];
-
-        if(data==false)
-        {
-            input.text="Wrong Input";
-        }
-
-        if(req.isNetworkError || req.isHttpError){
-            Debug.LogError(req.error);
-            yield break;
-        }
-
-        yield break;
+    IEnumerator CountDown(){
+        yield return new WaitForSeconds(20);
+        PlayerPrefs.SetInt("scanned",0);
     }
-    }*/
 }
